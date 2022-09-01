99916d2c913f45522670829e842cf024e1837cc7
fernanda-toledo2005@hotmail.com <93841326+Fernandaaptoledo@users.noreply.github.com>
2022-09-01 16:44:41 -0300

    programando

---
+++ Curso javascript/exer2.js
@@ -0,0 +1,6 @@
+
+let valorMaior = max (8,9);
+function max (numero1, numero2){
+    return numero1 > numero2? numero1: numero2;
+    console.log(valorMaior);
+}
--- mocha-chai/sample.spec.js
+++ mocha-chai/sample.spec.js
@@ -1,6 +1,8 @@
+import assert from 'assert'
+
 describe ('Teste1', () => {
 it ('igualdade', () => {
 let aux = 4
-
+assert.strictEqual(aux,4)
 })
 })
