4a4d9423828583fa51f3bd9ff37ea80f7d099c54
fernanda-toledo2005@hotmail.com <93841326+Fernandaaptoledo@users.noreply.github.com>
2022-09-02 00:08:49 -0300

    iniciar mocha-chai

---
+++ Curso javascript/exer6.js
@@ -0,0 +1,13 @@
+
+const filme = {
+    titulo: 'Duro de matar',
+    ano: 1988,
+    diretor: 'John Mc Tierman',
+    personagem: 'Bruce Willis'
+}
+exibirPropriedades(filme);
+function exibirPropriedades(obj) {
+    for (prop in obj)
+if (typeof prp === 'string')
+console.log(prp, obj [prop])
+}
